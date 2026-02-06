8d10c05e4550278c7d693a1966e4e32e816fe297
Ravish <ravishranjan.rr@gmail.com>
2026-02-06 13:28:29 +0530

    Deployed on GitHub

---
+++ Jspider/SpringBoot/Ultracare/Ultracare/src/main/java/com/hospital/Ultracare/UltracareApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.Ultracare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltracareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UltracareApplication.class, args);
+	}
+
+}
